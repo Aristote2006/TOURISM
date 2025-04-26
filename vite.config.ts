0d67a897e51d83593185af5293e45ff549943a19
Aristote2006 <cavakenneth58@gmail.com>
2025-04-26 17:39:49 +0300

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,10 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [
-    react(),
+    react({
+      jsxImportSource: 'react',
+      runtime: 'automatic'
+    }),
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
